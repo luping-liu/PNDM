e97facc5b9d039909e3cd75ff41d60dbe3aa8f20
sagarishere <5121817+sagarishere@users.noreply.github.com>
2023-02-09 01:28:02 +0200

    Update ema.py
    Remove the unnecessary if-else block for checking whether the module is a `nn.DataParallel` module. Since the shadow parameters are stored in a dictionary using the name of the parameters, this check is not necessary.

--- model/ema.py
+++ model/ema.py
@@ -49,18 +49,10 @@ class EMAHelper(object):
         for name, param in module.named_parameters():
             if param.requires_grad:
                 param.data.copy_(self.shadow[name].data)
-
+    
     def ema_copy(self, module):
-        if isinstance(module, nn.DataParallel):
-            inner_module = module.module
-            module_copy = type(inner_module)(
-                inner_module.config).to(inner_module.config.device)
-            module_copy.load_state_dict(inner_module.state_dict())
-            module_copy = nn.DataParallel(module_copy)
-        else:
-            module_copy = type(module)(module.config).to(module.config.device)
-            module_copy.load_state_dict(module.state_dict())
-        # module_copy = copy.deepcopy(module)
+        module_copy = type(module)(module.config).to(module.config.device)
+        module_copy.load_state_dict(module.state_dict())
         self.ema(module_copy)
         return module_copy
 
